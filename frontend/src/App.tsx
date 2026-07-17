7d1126a62b45019b0d8c43544199a7c713bcbdbc
PURECOS <2019pietcskunal86@poornima.org>
2026-07-17 14:09:50 +0530

    chore: initialize project architecture

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <div className="h-screen bg-black flex items-center justify-center">
+      <h1 className="text-red-600 text-6xl font-bold">
+        DriveFlix
+      </h1>
+    </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/constants/theme.ts
@@ -0,0 +1,7 @@
+export const COLORS = {
+  background: "#0B0B0B",
+  surface: "#141414",
+  primary: "#E50914",
+  secondary: "#FFFFFF",
+  textMuted: "#B3B3B3",
+}
